269b5cf08d3609ff0b375e96f67180133cb3e4e0
Kevin <kevinhamelink05@gmail.com>
2024-12-04 10:44:07 +0100

    Added update

--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -16,6 +17,11 @@ public class PlayerAttack : MonoBehaviour
         {
             Debug.LogError("No RigidBody2D on " + gameObject.name);
         }
+    }
+
+    private void Update()
+    {
+        Attack();
     }
 
     private void Attack()
