250a2aeb1fb29176fc39bf682df70ef7fe7baedf
Kevin <kevinhamelink05@gmail.com>
2024-12-05 13:09:19 +0100

    Fuck it we ball

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,33 +4,18 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [Header("Animation")]
-    //private Animator anim;
-    // private static readonly int Dash = Animator.StringToHash("Dash");
-    // private static readonly int Jump = Animator.StringToHash("Jump");
-    // private static readonly int Fall = Animator.StringToHash("Fall");
-    // private static readonly int Walk = Animator.StringToHash("Walk");
     private Rigidbody2D rb;
+    private PlayerState currentState = PlayerState.Idle;
 
-    private float jumpStartPos = 9999f;
-    private float elevationGained = 0f;
-    private bool isJumping = false;
-    private bool jumpKeyDown = false;
-    private bool jumpKeyUp = false;
-
-    [SerializeField] private float jumpHeight;
-    [SerializeField] private float size = 1;
-    [SerializeField] private float maxSpeed = 10f;
-    [SerializeField] private float jumpForce = 10f;
-
-    [Header("Dashing")] [SerializeField] private float dashingVelocity = 14f;
-    [SerializeField] private float dashDistance = 2f;
+    [Header("Dashing")]
+    [SerializeField] private bool isDashing;
+    [SerializeField] private bool canDash;
+    [SerializeField] private bool canTakeDamage = true;
     [SerializeField] private float dashingTime = 0.5f;
-
-    private Vector2 lastMovementDirection = new(1, 0);
-    private Vector2 dashingDirection;
-    private bool isDashing;
-    private bool canDash = true;
+    
+    [Header("Movement")]
+    [SerializeField] private float movementSpeed;
+    
 
 
     private void Start()
@@ -39,106 +24,73 @@ public class PlayerMovement : MonoBehaviour
         {
             Debug.LogError("No RigidBody2D on " + gameObject.name);
         }
-
-        // if (!TryGetComponent(out anim))
-        // {
-        //     Debug.LogError("No Animator on " + gameObject.name);
-        // }
     }
 
     private void Update()
     {
-        Movement();
-        if (!jumpKeyDown && Input.GetKeyDown(KeyCode.Space))
-        {
-            jumpKeyDown = true;
-        }
-
-        if (!jumpKeyUp && Input.GetKeyUp(KeyCode.Space))
-        {
-            jumpKeyUp = true;
-        }
-
-        var dashInput = Input.GetButtonDown("Dash");
-
-        if (!dashInput || !canDash) return;
-        isDashing = true;
-        canDash = false;
-        dashingDirection = new Vector2(Input.GetAxisRaw("Horizontal"), 0);
-        //anim.SetTrigger(Dash);
-        if (dashingDirection == Vector2.zero)
-        {
-            dashingDirection = lastMovementDirection;
-        }
-
-        StartCoroutine(StopDashing());
+        HandleMovement();
     }
 
-    private void FixedUpdate()
+    private void HandleMovement()
     {
-        if (rb == null) return;
-    }
-
-    private void Movement()
-    {
-        if (jumpKeyDown && IsGrounded())
+        if (isDashing) return;
+        if (Input.GetAxisRaw("Horizontal") >= 1)
         {
-            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-            jumpStartPos = transform.position.y;
-            isJumping = true;
-            //anim.SetTrigger(Jump);
-
-            print("Jump");
-            jumpKeyDown = false;
+            currentState = PlayerState.MovingRight;
         }
-
-        elevationGained = transform.position.y - jumpStartPos;
-        if (elevationGained > jumpHeight || jumpKeyUp)
+        else if (Input.GetAxisRaw("Horizontal") <= -1)
         {
-            print("Fall");
-            print("Elevation gained: " + elevationGained);
-            isJumping = false;
-            jumpKeyUp = false;
-            //anim.SetTrigger(Fall);
-        }
-
-        if (isDashing)
-        {
-            rb.velocity = dashingDirection.normalized * dashingVelocity;
-            isJumping = false;
+            currentState = PlayerState.MovingLeft;
         }
         else
         {
-            float moveX = Input.GetAxis("Horizontal") * maxSpeed;
-            if(math.abs(moveX) < 0.8f) moveX = 0;
-            Vector2 newVelocity;
-            newVelocity.x = moveX;
-            newVelocity.y = isJumping ? jumpForce : -jumpForce;
-            rb.velocity = newVelocity;
-            //anim.SetTrigger(Walk);
-
-            if (moveX != 0)
-            {
-                lastMovementDirection = new Vector2(moveX, 0);
-            }
+            currentState = PlayerState.Idle;
         }
 
-        if (IsGrounded())
+        if (Input.GetAxisRaw("Fire3") >= 1 && canDash && !isDashing)
         {
-            canDash = true;
+            // Perform dashing
+        }
+        HandleState();
+    }
+
+    private void HandleState()
+    {
+        switch (currentState)
+        {
+            case PlayerState.MovingLeft:
+                rb.velocity = new Vector2(-movementSpeed, rb.velocity.y);
+                break;
+            case PlayerState.MovingRight:
+                rb.velocity = new Vector2(movementSpeed, rb.velocity.y);
+                break;
+            case PlayerState.Dashing:
+                // Make dashing logic
+                break;
+            case PlayerState.Idle:
+                rb.velocity = new Vector2(0, rb.velocity.y);
+                break;
+            default:
+                rb.velocity = new Vector2(0, rb.velocity.y);
+                break;
         }
     }
-
-    private IEnumerator StopDashing()
+    
+    
+    // make dashing method
+    public void Dash()
     {
-        yield return new WaitForSeconds(dashingTime);
-        isDashing = false;
+        
     }
 
-    private bool IsGrounded()
+    private enum PlayerState
     {
-        Debug.DrawRay(transform.position + Vector3.down * size, Vector2.down * 2f, Color.red);
-        return Physics2D.Raycast(transform.position + Vector3.down * size, Vector2.down, 0.1f).collider != null &&
-               rb.velocity.y == 0;
+        Idle,
+        MovingLeft,
+        MovingRight,
+        FallingLeft,
+        FallingRight,
+        Jumping,
+        Dashing
     }
 }
